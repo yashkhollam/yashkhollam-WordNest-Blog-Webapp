298123b35b2ea47ff174f021acbee56c0a3ba0f0
yashkhollam <yashkhollam01@gmail.com>
2025-09-30 17:47:34 +0530

    update the DB

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
    res.status(200).json({
-    message:"Welcome to the blof app"
+    message:"Welcome to the Wordnest Webapp"
    })
 })
 
